f72d4cc372cb80c4b307c85ea6ce8fd909242902
Mohammad AL-Ali <m.alali.sy@gmail.com>
2020-03-23 22:19:45 +0300

    feat: the base implementation for SymbolTable

---
+++ src/executors/languages_executors/es/SymbolTable.ts
@@ -0,0 +1,44 @@
+enum EntryType {
+    VARIABLE
+}
+
+class TableEntry {
+    type: EntryType;
+    value: any;
+
+    constructor(type: EntryType, value: any) {
+        this.type = type;
+        this.value = value;
+    }
+}
+
+export class AlreadyDefinedIdentifier extends Error {
+
+}
+
+export default class SymbolTable {
+
+    parent?: SymbolTable;
+    entries: any;
+
+    constructor(parent?: SymbolTable) {
+        this.parent = parent;
+        this.entries = {};
+    }
+
+    defineVariable(name: string, value: any = undefined) {
+        if (this.entries[name]) {
+            throw new AlreadyDefinedIdentifier();
+        }
+        this.entries[name] = new TableEntry(EntryType.VARIABLE, value);
+    }
+
+    getValue(name: string) : any {
+        if(this.entries[name] !== undefined)
+            return this.entries[name];
+        if(this.parent) return this.parent.getValue(name);
+        return undefined;
+    }
+
+
+}
