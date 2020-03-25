73b3179eee9cc9bbda165a3d3b9657f0861e89b7
Mohammad AL-Ali <m.alali.sy@gmail.com>
2020-03-26 01:51:54 +0300

    implement the code editor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,93 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import MonacoEditor from "react-monaco-editor";
 import {editor} from "monaco-editor";
+import {Button, ButtonGroup, Col, Container, Row} from "react-bootstrap";
+import JavaScriptExecutor from "./executors/languages_executors/es/EcmaScriptExecutor";
+import ErrorHandler from "./executors/errorhandler/ErrorHandler";
+import Error from "./executors/errorhandler/Error";
+import Action from "./executors/actions/Action";
 
 
 function App() {
 
-  let editor:editor.IStandaloneCodeEditor;
-  let monaco:any;
+    let editor: editor.IStandaloneCodeEditor;
+    let monaco: any;
 
-  let decorator : string[] = [];
+    let decorator: string[] = [];
 
-  let [executionState, setExecutionState] = useState({line: 1, variables: []});
+    let [executionState, setExecutionState] = useState({line: -1, variables: []});
+    let [codeExecuted, setCodeExecuted] = useState(false);
+    let [fatalError, setFatalError] = useState(false);
+    let [lastActionIndex, setLastActionIndex] = useState(0);
+    let [errors, setErrors] = useState([]);
+    let [actions, setActions] = useState(new Array<Action>());
 
-  let editorDidMount = (ed:editor.IStandaloneCodeEditor, mon:any) => {
-    ed.focus();
+    let editorDidMount = (ed: editor.IStandaloneCodeEditor, mon: any) => {
+        ed.focus();
 
-    editor = ed;
-    monaco = mon;
-  };
+        editor = ed;
+        monaco = mon;
+    };
 
-  useEffect(()=>{
-    decorator = editor.deltaDecorations(decorator, [
-      {
-        range: new monaco.Range(executionState.line, 1, executionState.line, 1),
-        options: {
-          isWholeLine: true,
-          className: 'lineDecorator',
+    useEffect(() => {
+        if (!codeExecuted) return;
+        decorator = editor?.deltaDecorations(decorator, [
+            {
+                range: new monaco.Range(executionState.line, 1, executionState.line, 1),
+                options: {
+                    isWholeLine: true,
+                    className: 'lineDecorator',
+                    glyphMarginClassName: 'lineDecorator'
+                }
+            }
+        ]);
+    }, [executionState]);
+
+    let errorHandler = new class ErrorHandler {
+        handleError(error: Error): void {
+            if (error.fatal) setFatalError(true);
         }
-      }
-    ]);
-  }, [executionState]);
+    };
 
+    let onExecuteClicked = () => {
+        if (editor.getModel()?.getValue() == null) return;
+        setErrors([]);
+        setLastActionIndex(0);
+        setCodeExecuted(true);
+        setFatalError(false);
+        let executor = new JavaScriptExecutor(editor.getModel()?.getValue()!!, errorHandler);
+        setActions(executor.executeAll());
+        setExecutionState({...executionState, line: 1});
+    };
 
-  return (
-        <MonacoEditor
-            language="javascript"
-            height={500}
-            width={"40%"}
-            value={"var x = 3; \nfunction(){}\nif(x == 4) {\n\tx += 4;\n}"}
-            editorDidMount={editorDidMount}
+    return (
 
-        />
+        <Container>
+            <Row className={"justify-content-center"}>
+                <Col sm={12} md={6}>
+                    <div className={"code-container"}>
+                        <MonacoEditor
+                            language="javascript"
+                            height={500}
+                            width={'100%'}
+                            value={"var x = 3; \nfunction(){}\nif(x == 4) {\n\tx += 4;\n}"}
+                            editorDidMount={editorDidMount}
+                        />
+                    </div>
+                    <Row className={"justify-content-center"}>
+                        <Button onClick={onExecuteClicked}
+                                className={"m-3"}>{codeExecuted ? "Reset" : "Execute"}</Button>
+                    </Row>
+
+                </Col>
+                <Col sm={12} md={6}>
+
+                </Col>
+            </Row>
+        </Container>
+
     );
 }
 
